b8ca6461f1e1417c50d60c6cdb8929cb0da9d6ba
yukilaumsolution <108311997+yukilaumsolution@users.noreply.github.com>
2022-11-17 16:19:06 +0800

    Add files via upload

---
+++ config.js
@@ -0,0 +1,10 @@
+const config = {
+    dbTaiPo: {
+        host: "aurora-database-1-instance-1.cych1nsrjcyp.ap-southeast-1.rds.amazonaws.com",
+        user: "msolution",
+        password: "msolution256",
+        database: "bas_dev"
+    }
+}
+
+module.exports = config
